16c994e4a3cdbecbab522757a0180a034e1b9ad7
walway <Wayne.walsh@rymanhealthcare.com>
2020-11-12 14:31:46 +1300

    Added Ms Tests

---
+++ SampleTests/Tests/MsUserAuthTest.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SeleniumBase.Framework.Core.Helpers;
+using Microsoft.Identity.Client;
+
+namespace SampleTests.Tests
+{
+    [TestClass]
+    public class MsUserAuthTest
+    {
+        private static string AuthUrl = "https://login.microsoftonline.com/c92c5826-4969-414a-a289-1409683ebac0/oauth2/v2.0/authorize";
+      [TestMethod]
+        public async void MsLoginTest()
+        {
+           await MicrosoftLoginHelper.GetATokenForMSUser(AuthUrl);
+
+
+        }
+
+    }
+}
